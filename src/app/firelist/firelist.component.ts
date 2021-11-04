25227a39f22add9a7821d753e6e1a692e272f0a7
isabeela <isabeelabg@gmail.com>
2021-11-04 19:34:48 -0300

    2 files modified

--- src/app/firelist/firelist.component.ts
+++ src/app/firelist/firelist.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-firelist',
   templateUrl: './firelist.component.html',
-  styleUrls: ['./firelist.component.css']
+  styleUrls: ['./firelist.component.css'],
 })
 export class FirelistComponent implements OnInit {
+  nome: string;
+  idade: number;
 
-  constructor() { }
+  listRef: AngularFireList<any>;
+  list: Observable<any[]>;
 
-  ngOnInit() {
+  constructor(
+    private db: AngularFireDatabase,
+    private authService: AuthService
+  ) {
+    this.listRef = db.list('list');
+    this.list = this.listRef
+      .snapshotChanges()
+      .pipe(
+        map((changes) =>
+          changes.map((c) => ({ key: c.payload.key, ...c.payload.val() }))
+        )
+      );
   }
 
+  ngOnInit() {}
+
+  addItem() {
+    this.listRef.push({
+      nome: this.nome,
+      idade: this.idade,
+      email: this.authService.firebaseAuth,
+    });
+    this.nome = null;
+    this.idade = null;
+  }
+
+  deleteItem(key: string) {
+    this.listRef.remove(key);
+  }
 }
